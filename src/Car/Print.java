5e2416b06e120eb222c903f8b3225d149544f8bd
imthe <imtherealsebastian@gmail.com>
2020-05-02 19:17:35 +0300

    # WARNING: head commit changed in the meantime
    Changed PRINT

--- src/Car/Print.java
+++ src/Car/Print.java
@@ -1,19 +1,8 @@
 package Car;
-/*package Car;
-
-import database.InventoryDB;
-import database.Serializer;
-
 public class Print {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Serializer serializer = new Serializer();
-
-		InventoryDB inventoryDB = (InventoryDB)serializer.deserialize("InventoryDB");
-		
-		System.out.println(inventoryDB.toString());
+		// HELLO MY NAME IS SEBAS
 	}
 
 }
-*/
