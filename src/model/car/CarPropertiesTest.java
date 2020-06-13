86ce99d090451237ef91c96849597e449c5ab90c
MythProg <etay.rab@gmail.com>
2020-06-13 13:27:51 +0300

    add CarPropertiesTest Tests

--- src/model/car/CarPropertiesTest.java
+++ src/model/car/CarPropertiesTest.java
@@ -2,38 +2,70 @@ package model.car;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import model.car.CarProperties;
+import model.database.CarPropertiesDB;
+import model.database.Serializer;
+import model.database_initializers.CarPropertiesInitializer;
+
 class CarPropertiesTest extends CarProperties {
+		
+		public static CarProperties carProp;
+		public static Map<String, Integer> iTest_trims;
+		public static List<String> iTest_colors;
 
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-	}
+		@BeforeAll
+		static void setUpBeforeClass() throws Exception {
+		}
 
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
-	}
+		@AfterAll
+		static void tearDownAfterClass() throws Exception {
+		}
 
-	@BeforeEach
-	void setUp() throws Exception {
-	}
+		@BeforeEach
+		void setUp() throws Exception {
+			iTest_trims = new HashMap<String,Integer>();
+			iTest_trims.put("Intense", 69900); 
+			iTest_colors = new ArrayList<String>();
+			iTest_colors.add("Polar White");
+			carProp = new CarProperties("Compact","Hyundai","iTest", iTest_trims,iTest_colors);
+		}
 
-	@AfterEach
-	void tearDown() throws Exception {
-	}
+		@AfterEach
+		void tearDown() throws Exception {
+			carProp=null;
+		}	
 
-	@Test
-	void testSetTrims() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testSetColors() {
-		fail("Not yet implemented");
-	}
+		@Test
+		void testSetTrims() {
+			Map<String, Integer> iTest1_trims = new HashMap<String,Integer>();
+			iTest1_trims.put("Intence", -15);
+			carProp.setTrims(iTest1_trims);
+			Map<String,Integer> expected = CarPropertiesTest.iTest_trims;
+			Map<String,Integer> actual = carProp.getTrims();
+			assertEquals(expected, actual);
+		}
+		
+	
+		@Test
+		void testSetColors() {
+			List<String> iTest1_colors = new ArrayList<String>();
+			iTest1_colors.addAll(carProp.getColors());
+			iTest1_colors.add("Polar White");
+			carProp.setColors(iTest1_colors);
+			List<String> expected = CarPropertiesTest.iTest_colors;
+			List<String> actual = carProp.getColors();
+			assertEquals(expected, actual);		
+			}
 
 }
